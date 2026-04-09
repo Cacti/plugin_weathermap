9cea83391acbf9940a0835825332e9ae6f997f1c
Thomas Vincent <thomasvincent@gmail.com>
2026-04-09 01:27:25 -0700

    fix(security): escape user data before DOM insertion to prevent XSS
    Signed-off-by: Thomas Vincent <thomasvincent@gmail.com>

--- js/editor.js
+++ js/editor.js
@@ -1,4 +1,11 @@
 // global variable for subwindow reference
+// Escape HTML special characters to prevent XSS
+function escapeHtml(str) {
+	if (typeof str !== 'string') return '';
+	return str.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;').replace(/'/g, '&#039;');
+}
+
+
 const MESSAGE_LEVEL_NONE  = 0;
 const MESSAGE_LEVEL_INFO  = 1;
 const MESSAGE_LEVEL_WARN  = 2;
@@ -1050,11 +1057,11 @@ function prime_link_form(name) {
 
 		// if that didn't 'stick', then we need to add the special value
 		if ($('#link_commentposout').val() != mylink.commentposout) {
-			$('#link_commentposout').prepend("<option selected value='" + mylink.commentposout + "'>" + mylink.commentposout + "%</option>");
+			$('#link_commentposout').prepend($('<option>', { selected: true, value: mylink.commentposout, text: mylink.commentposout + '%' }));
 		}
 
 		if ($('#link_commentposin').val() != mylink.commentposin) {
-			$('#link_commentposin').prepend("<option selected value='" + mylink.commentposin + "'>" + mylink.commentposin + "%</option>");
+			$('#link_commentposin').prepend($('<option>', { selected: true, value: mylink.commentposin, text: mylink.commentposin + '%' }));
 		}
 
 		document.getElementById('link_nodename1').firstChild.nodeValue  = mylink.a;
