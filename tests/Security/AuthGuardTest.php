10420ad0190f12d32fce1b3ef22367613560e30c
Thomas Vincent <thomasvincent@gmail.com>
2026-04-10 07:06:40 -0700

    fix(tests): escape quotes in Pest regex patterns
    Signed-off-by: Thomas Vincent <thomasvincent@gmail.com>

--- tests/Security/AuthGuardTest.php
+++ tests/Security/AuthGuardTest.php
@@ -57,7 +57,7 @@ describe('auth guard presence in weathermap', function () {
 			if ($contents === false) continue;
 
 			// Check for get_filter_request_var usage for numeric IDs
-			if (preg_match('/get_request_var\s*\(\s*['"]id['"]/', $contents)) {
+			if (preg_match('/get_request_var\s*\(\s*[\'\"]id[\'\"]/', $contents)) {
 				// Should use get_filter_request_var for 'id' params
 				$hasFilter = (
 					strpos($contents, 'get_filter_request_var') !== false ||
--- tests/Security/RedirectSafetyTest.php
+++ tests/Security/RedirectSafetyTest.php
@@ -28,7 +28,7 @@ describe('redirect safety in weathermap', function () {
 			$missingExit = 0;
 
 			for ($i = 0; $i < count($lines); $i++) {
-				if (preg_match('/header\s*\(\s*['"]Location/', $lines[$i])) {
+				if (preg_match('/header\s*\(\s*[\'\"]Location/', $lines[$i])) {
 					// Next non-empty line should contain exit, die, or return
 					$foundExit = false;
 					for ($j = $i + 1; $j < min($i + 4, count($lines)); $j++) {
