90f2f624f25cfb7a753190e4f77afc658e5d7434
Thomas Vincent <thomasvincent@gmail.com>
2026-04-10 07:01:32 -0700

    style: remove trailing double semicolons
    Signed-off-by: Thomas Vincent <thomasvincent@gmail.com>

--- lib/WeatherMapLink.class.php
+++ lib/WeatherMapLink.class.php
@@ -272,7 +272,7 @@ class WeatherMapLink extends WeatherMapItem {
 			}
 
 			if ($comment != '') {
-				// print "\n\n----------------------------------------------------------------\nComment $dir for ".$this->name."\n";;
+				// print "\n\n----------------------------------------------------------------\nComment $dir for ".$this->name."\n";
 
 				[$textlength, $textheight] = $this->owner->myimagestringsize($this->commentfont, $comment);
 
