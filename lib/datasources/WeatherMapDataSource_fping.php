46cde866cae62377fc46ebb165dc52ec3be2eeea
Thomas Vincent <thomasvincent@gmail.com>
2026-05-17 04:03:18 -0700

    harden(weathermap): escape fping binary path in command construction
    Signed-off-by: Thomas Vincent <thomasvincent@gmail.com>

--- lib/datasources/WeatherMapDataSource_fping.php
+++ lib/datasources/WeatherMapDataSource_fping.php
@@ -100,7 +100,7 @@ class WeatherMapDataSource_fping extends WeatherMapDataSource {
 			$pattern .= '/';
 
 			if (is_executable($this->fping_cmd)) {
-				$command = $this->fping_cmd . ' -t100 -r1 -p20 -u -C ' . (int) $ping_count . ' -i10 -q ' . cacti_escapeshellarg($target) . ' 2>&1'; // nosemgrep: php.lang.security.exec-use.exec-use -- fping_cmd is admin-configured; target validated against fping: pattern
+				$command = cacti_escapeshellarg($this->fping_cmd) . ' -t100 -r1 -p20 -u -C ' . (int) $ping_count . ' -i10 -q ' . cacti_escapeshellarg($target) . ' 2>&1'; // nosemgrep: php.lang.security.exec-use.exec-use -- fping_cmd is admin-configured; target validated against fping: pattern
 
 				wm_debug("Running $command");
 				$pipe = popen($command, 'r'); // nosemgrep: php.lang.security.exec-use.exec-use -- fping_cmd is admin-configured; target validated above via cacti_escapeshellarg
